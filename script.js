ba8aa4bd063e3ea10a2bc081e6293de1a7af110d
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-11-09 15:04:10 +0100

    feat: if one person is removed, it will now be replaced by another

--- script.js
+++ script.js
@@ -4,7 +4,57 @@ const contactArray = [];
 let personNumber = -1;
 let pending = 0;
 
-function addnewPerson() {}
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function addnewPerson(xxx) {
+  console.log(xxx);
+  console.log(xxx[0]);
+  console.log(typeof xxx[0]);
+  const newDiv = document.createElement("div");
+  const dateString = Date.now().toString(36);
+  console.log(dateString);
+  const randomness = Math.random().toString(36);
+  console.log(randomness);
+  console.log(dateString + randomness);
+  newDiv.id = dateString + randomness;
+  newDiv.classList.add("divPerson");
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", xxx[0].picture);
+  newDiv.appendChild(newImage);
+  const newName = document.createElement("p");
+  newName.innerText = `${xxx[0].name.title}. ${xxx[0].name.first} ${xxx[0].name.last}`;
+  newDiv.appendChild(newName);
+  const newTitle = document.createElement("p");
+  newTitle.innerText = xxx[0].title;
+  newDiv.appendChild(newTitle);
+  const connections = document.createElement("p");
+  connections.innerText = `${xxx[0].mutualConnections} mutual Connections`;
+  newDiv.appendChild(connections);
+  const newConnectButton = document.createElement("button");
+  newConnectButton.innerText = "Connect";
+  newConnectButton.classList.add("connectButton");
+  newDiv.appendChild(newConnectButton);
+  const newCloseButton = document.createElement("button");
+  newCloseButton.innerText = "x";
+  newCloseButton.classList.add("closeButton");
+  newDiv.appendChild(newCloseButton);
+  personsContainer.appendChild(newDiv);
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function renderContacts() {
+  for (let i = 0; i < contactArray.length; i++) {
+    if (i === 0) {
+      personsContainer.innerHTML = "";
+      personsContainer.classList.add("personsContainerAfterLoading");
+    }
+    addnewPerson(contactArray[i]);
+  }
+  console.log(contactArray);
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 function fetchNewPerson() {
   fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=1")
@@ -18,55 +68,49 @@ function fetchNewPerson() {
       console.log(contactArray);
       personNumber++;
       console.log(personNumber);
-      if (personNumber === 7) {
-        for (let i = 0; i < contactArray.length; i++) {
-          if (i === 0) {
-            personsContainer.innerHTML = "";
-            personsContainer.classList.add("personsContainerAfterLoading");
-          }
-          console.log(contactArray[i]);
-          console.log(contactArray[i][0]);
-          console.log(typeof contactArray[i][0]);
-          const newDiv = document.createElement("div");
-          const dateString = Date.now().toString(36);
-          console.log(dateString);
-          const randomness = Math.random().toString(36);
-          console.log(randomness);
-          console.log(dateString + randomness);
-          newDiv.id = dateString + randomness;
-          newDiv.classList.add("divPerson");
-          const newImage = document.createElement("img");
-          newImage.setAttribute("src", contactArray[i][0].picture);
-          newDiv.appendChild(newImage);
-          const newName = document.createElement("p");
-          newName.innerText = `${contactArray[i][0].name.title}. ${contactArray[i][0].name.first} ${contactArray[i][0].name.last}`;
-          newDiv.appendChild(newName);
-          const newTitle = document.createElement("p");
-          newTitle.innerText = contactArray[i][0].title;
-          newDiv.appendChild(newTitle);
-          const connections = document.createElement("p");
-          connections.innerText = `${contactArray[i][0].mutualConnections} mutual Connections`;
-          newDiv.appendChild(connections);
-          const newConnectButton = document.createElement("button");
-          newConnectButton.innerText = "Connect";
-          newConnectButton.classList.add("connectButton");
-          newDiv.appendChild(newConnectButton);
-          const newCloseButton = document.createElement("button");
-          newCloseButton.innerText = "x";
-          newCloseButton.classList.add("closeButton");
-          newDiv.appendChild(newCloseButton);
-          personsContainer.appendChild(newDiv);
-        }
-      }
+      renderContacts();
     });
 }
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 for (let i = 0; i < 8; i++) {
   fetchNewPerson();
 }
 
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 personsContainer.addEventListener("click", function (event) {
   console.log(event.target);
-  console.log(event.target.parentElement);
+  const parentDiv = event.target.parentElement;
+  console.log(parentDiv);
   personsContainer.removeChild(event.target.parentElement);
+  const fullName = parentDiv.querySelector("p").innerText;
+  console.log(fullName);
+  const fullNameSplitted = fullName.split(" ");
+  console.log(fullNameSplitted);
+  const firstName = fullNameSplitted[1];
+  console.log(firstName);
+  const lastName = fullNameSplitted[2];
+  console.log(lastName);
+  console.log(contactArray);
+  let indexOfPersonToDelete;
+  for (let person of contactArray) {
+    if (
+      person[0].name.first === firstName &&
+      person[0].name.last === lastName
+    ) {
+      console.log(contactArray.indexOf(person));
+      indexOfPersonToDelete = contactArray.indexOf(person);
+    }
+  }
+  console.log(indexOfPersonToDelete);
+  // const copy = structuredClone(contactArray);
+  // console.log(copy);
+  contactArray.splice(indexOfPersonToDelete, 1);
+  console.log(contactArray);
+  personNumber--;
+  fetchNewPerson();
 });
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
