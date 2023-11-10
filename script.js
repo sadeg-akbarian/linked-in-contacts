bc1cdf22f188eb70a7ca8bb58e232d2a89b832c8
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-11-10 15:12:07 +0100

    feat: for-loop in renderContacts-function removed, pending-functionalities implemented, project fully completed

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const personsContainer = document.querySelector("#personsContainer");
+const displayNumberOfPendings = document.querySelector("#numberOfPendings");
 
 const contactArray = [];
 let personNumber = -1;
 let pending = 0;
+let pendingDisplayed;
+let checkedPersonID = "";
+
+const initialNumberOfPendings = JSON.parse(
+  localStorage.getItem("NumberOfPendings")
+);
+console.log(initialNumberOfPendings);
+if (initialNumberOfPendings !== null) {
+  pending = initialNumberOfPendings;
+}
 
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -13,7 +24,7 @@ function displayNewPerson(xxx) {
   const newDiv = document.createElement("div");
   const dateString = Date.now().toString(36);
   console.log(dateString);
-  const randomness = Math.random().toString(36);
+  const randomness = Math.random().toString(36).substring(2);
   console.log(randomness);
   console.log(dateString + randomness);
   newDiv.id = dateString + randomness;
@@ -44,13 +55,20 @@ function displayNewPerson(xxx) {
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 function renderContacts() {
-  for (let i = 0; i < contactArray.length; i++) {
-    if (i === 0) {
-      personsContainer.innerHTML = "";
-      personsContainer.classList.add("personsContainerAfterLoading");
+  if (contactArray.length === 1) {
+    if (pending === 0) {
+      pendingDisplayed = "No";
+    } else {
+      pendingDisplayed = pending;
     }
-    displayNewPerson(contactArray[i]);
+    console.log(pendingDisplayed);
+    displayNumberOfPendings.innerText =
+      pendingDisplayed + " pendings invitations";
+    personsContainer.innerHTML = "";
+    personsContainer.classList.add("personsContainerAfterLoading");
   }
+  displayNewPerson(contactArray[contactArray.length - 1]);
+
   console.log(contactArray);
 }
 
@@ -82,35 +100,74 @@ for (let i = 0; i < 8; i++) {
 
 personsContainer.addEventListener("click", function (event) {
   console.log(event.target);
-  const parentDiv = event.target.parentElement;
-  console.log(parentDiv);
-  personsContainer.removeChild(event.target.parentElement);
-  const fullName = parentDiv.querySelector("p").innerText;
-  console.log(fullName);
-  const fullNameSplitted = fullName.split(" ");
-  console.log(fullNameSplitted);
-  const firstName = fullNameSplitted[1];
-  console.log(firstName);
-  const lastName = fullNameSplitted[2];
-  console.log(lastName);
-  console.log(contactArray);
-  let indexOfPersonToDelete;
-  for (let person of contactArray) {
-    if (
-      person[0].name.first === firstName &&
-      person[0].name.last === lastName
-    ) {
-      console.log(contactArray.indexOf(person));
-      indexOfPersonToDelete = contactArray.indexOf(person);
+  console.log(event.target.classList[0]);
+  if (event.target.classList[0] === "closeButton") {
+    console.log("yeeeeeeeeessssssssssssssss");
+    const parentDiv = event.target.parentElement;
+    console.log(parentDiv);
+    // personsContainer.removeChild(event.target.parentElement);
+    const fullName = parentDiv.querySelector("p").innerText;
+    console.log(fullName);
+    const fullNameSplitted = fullName.split(" ");
+    console.log(fullNameSplitted);
+    const firstName = fullNameSplitted[1];
+    console.log(firstName);
+    const lastName = fullNameSplitted[2];
+    console.log(lastName);
+    console.log(contactArray);
+    let indexOfPersonToDelete;
+    for (let person of contactArray) {
+      if (
+        person[0].name.first === firstName &&
+        person[0].name.last === lastName
+      ) {
+        console.log(contactArray.indexOf(person));
+        indexOfPersonToDelete = contactArray.indexOf(person);
+      }
     }
+    console.log(indexOfPersonToDelete);
+    contactArray.splice(indexOfPersonToDelete, 1);
+    console.log(contactArray);
+    personNumber--;
+    fetchNewPerson();
+  } else if (event.target.classList[0] === "connectButton") {
+    console.log("Genaaaaaauuuuuuu");
+    console.log(event.target.innerText);
+    console.log(event.target.parentElement.id);
+    console.log(typeof event.target.parentElement.id);
+    if (event.target.innerText === "Connect") {
+      pending++;
+    } else {
+      pending--;
+    }
+    localStorage.setItem("NumberOfPendings", JSON.stringify(pending));
+    checkedPersonID = event.target.parentElement.id;
+    renderPendingState();
   }
-  console.log(indexOfPersonToDelete);
-  // const copy = structuredClone(contactArray);
-  // console.log(copy);
-  contactArray.splice(indexOfPersonToDelete, 1);
-  console.log(contactArray);
-  personNumber--;
-  fetchNewPerson();
 });
 
 // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function renderPendingState() {
+  console.log(pending);
+  console.log(checkedPersonID);
+  if (pending === 0) {
+    pendingDisplayed = "No";
+    console.log("ääääääääääääääääääääääääääääää");
+  } else {
+    pendingDisplayed = pending;
+    console.log("ääääääääääääääääääääääääääääää");
+  }
+  displayNumberOfPendings.innerText =
+    pendingDisplayed + " pendings invitations";
+  const checkedPerson = document.querySelector("#" + checkedPersonID);
+  console.log(checkedPerson);
+  const clickedConnectButton = checkedPerson.querySelector(".connectButton");
+  console.log(clickedConnectButton);
+  console.log(clickedConnectButton.innerText);
+  if (clickedConnectButton.innerText === "Connect") {
+    clickedConnectButton.innerText = "Pending";
+  } else {
+    clickedConnectButton.innerText = "Connect";
+  }
+}
